59cade73417c9c81f37395944afeae823203b983
Adelina87 <ioana.pascal@gmail.com>
2016-06-13 16:38:57 -0400

    Update Plot1.R

--- Data Plotting Assignment/Plot1.R
+++ Data Plotting Assignment/Plot1.R
@@ -1,7 +1,10 @@
+##setting directory and reading the file
 myfile <- file("C:/Data Science/Data Plotting Assignment/household_power_consumption.txt")
 data <- read.table(text=grep("^[1,2]/2/2007", readLines(file),value=TRUE),
                    col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
                    sep=";", header=TRUE)
+                   
+##Plot 1
 png(file="Data Plotting Assignment/Plot1.png")
 hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
