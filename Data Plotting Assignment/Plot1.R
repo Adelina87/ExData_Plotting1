da2816510855fecd786ecc4419d6104fd85dee7f
Adelina87 <ioana.pascal@gmail.com>
2016-06-13 15:58:31 -0400

    Add files via upload

--- Data Plotting Assignment/Plot1.R
+++ Data Plotting Assignment/Plot1.R
@@ -2,6 +2,6 @@ myfile <- file("C:/Data Science/Data Plotting Assignment/household_power_consump
 data <- read.table(text=grep("^[1,2]/2/2007", readLines(file),value=TRUE),
                    col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
                    sep=";", header=TRUE)
-png(file="Plot1.png")
+png(file="Data Plotting Assignment/Plot1.png")
 hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
